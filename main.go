bed2a73a78bf83266849f7754ff084782df6a523
mrkhdev <someone@sdfiuu3.com>
2025-01-29 15:23:48 +0100

    ci: Add version and commit information to main application
    - Introduce Version and Commit variables in main.go
    - Update main function to print version and commit details
    - Configure GitHub Actions to dynamically generate version tags

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var (
+	Version = "v0.0.0"
+	Commit  = "none"
+)
+
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Printf("Hello, World from version %s (commit: %s)! \n", Version, Commit)
 }
